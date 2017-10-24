59aef88cd9e65561a634e4b19f732651d189cb79
mikenic <temperanceaubie@gmail.com>
2017-10-24 00:57:55 -0700

    add friends and search

---
+++ app/models/user.rb
@@ -0,0 +1,45 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  	has_many :friendships  
+  	has_many :friends, through: :friendships     
+
+	def full_name
+    	return "#{first_name} #{last_name}".strip if (first_name || last_name) 
+    	"Anonymous"
+  	end
+
+	def not_friends_with?(friend_id)
+		friendships.where(friend_id: friend_id).count < 1
+	end
+
+	def except_current_user(users)
+		users.reject {|user| user.id == self.id}
+	end
+
+	def self.search(param)
+		return User.none if param.blank?
+		param.strip!
+		param.downcase!
+		(first_name_matches(param) + last_name_matches(param) + email_matches(param)).uniq
+	end
+
+	def self.first_name_matches(param)
+		matches('first_name', param)
+	end
+
+	def self.last_name_matches(param)
+		matches('last_name', param)
+	end
+
+	def self.email_matches(param)
+		matches('email', param)
+	end
+
+	def self.matches(field_name, param)
+		where("lower(#{field_name}) like ?", "%#{param}%")
+	end
+end
---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  	devise_for :users
+
+  	resources :users, only: [:show]
+  	resources :friendships
+
+  	root 'welcome#index'
+	get 'my_friends', to: 'users#my_friends'
+	get 'search_friends', to: 'users#search'
+	post 'add_friend', to: 'users#add_friend'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
