fc7972e21f968585f95621285b972535c918419d
mikenic <temperanceaubie@gmail.com>
2017-11-02 21:21:13 -0700

    fixes

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,7 +1,7 @@
 module Api
 	module V1
 		class PostsController < ApplicationController
-			skip_before_action :verify_authenticity_token
+			#skip_before_action :verify_authenticity_token
 			respond_to :json
 
 
