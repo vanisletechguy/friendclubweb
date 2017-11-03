92f2a8a33f329c2956fc1070522373abc323d1f7
mikenic <temperanceaubie@gmail.com>
2017-11-02 20:05:24 -0700

    token verify in api

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -1,9 +1,11 @@
 module Api
   module V1
-    class ApplicationController < ApplicationController
-      skip_before_action :verify_authenticity_token
-      acts_as_token_authentication_handler_for User
-      include DeviseTokenAuth::Concerns::SetUserByToken
+    class ApplicationController < ActionController::Base
+      #skip_before_action :verify_authenticity_token
+      #acts_as_token_authentication_handler_for User
+      #include DeviseTokenAuth::Concerns::SetUserByToken
+
+      before_action :authenticate_request
 
 		attr_reader :current_user 
 
@@ -18,3 +20,7 @@ module Api
     end
   end
 end
+
+
+#eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoyLCJleHAiOjE1MDk3NjM3NDd9.zB4C7zHSd_MG-uCq7qPvXUx6sZM3eAV0QuQdZj3g4I8
+# curl -H "Authorization: eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoyLCJleHAiOjE1MDk3NjM3NDd9.zB4C7zHSd_MG-uCq7qPvXUx6sZM3eAV0QuQdZj3g4I8" http://localhost:3000/api/v1/my_friends
