edcba09c127c551503c8747b32c1c1828cea066f
mikenic <temperanceaubie@gmail.com>
2017-11-06 03:48:34 -0800

    carrierwave2

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -26,4 +26,6 @@ CarrierWave.configure do |config|
   #config.fog_host       = 'https://assets.example.com'           # optional, defaults to nil
   #config.fog_public     = false                                  # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+	storage :fog
+
 end
