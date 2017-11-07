46bb4a0962a308d6f6cc9f6eb195048cd7279688
mikenic <temperanceaubie@gmail.com>
2017-11-07 02:42:09 -0800

    api fixes

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -7,37 +7,28 @@ module Api
 			def index
 				#all posts
 				#respond_with Post.all
-				
 				@posts = Post.all
 				render json: {status: 'SUCCESS', message:'Loaded Posts', data:@posts}, status: :ok
-
 			end
 
 			def show
 				#respond_with Post.find(params[:id])
-
 				@post = Post.find(params[:id])
 				render json: {status: 'SUCCESS', message:'Loaded Post', data:@post}, status: :ok
-
 			end
 
 			def create
-				#respond_with Post.create(params[:friendship])
-			
+				#respond_with Post.create(params[:friendship])			
 				#@user = User.where(email: params[:user]).first
-				
-
 				##@all_users = User.all
 				##@api_user = @all_users.find_by_email(params[:email])
-				
-
 				#@api_user = @all_users.first
 				#if (@user = User.find_by_email(params[:user][:email])) && @user.valid_password?(params[:user][:password])
-
 				@api_user = @current_user
 				if @api_user
 					@post = new Post(post_params)
-					@api_user.posts.build(@post)
+					#@api_user.posts.build(@post)
+					@post_image = decode_image_data(post_params[:image_data])
 					#@post = @api_user.posts.build(post_params)
 					if @post.save
 						render json: {status: 'SUCCESS', message:'Saved Post', data:@post}, status: :ok
@@ -60,7 +51,7 @@ module Api
 			def post_params
 				json = JSON.parse(request.raw_post)
 				params = ActionController::Parameters.new(json)
-				params.permit(:title, :content, :longitude, :latitude, :image_data)### fix
+				params.permit(:title, :content, :longitude, :latitude, :image_data, :image)### fix
 				params[:image] = decode_image_data(params[:image_data])
 			end
 
