103ea58cd10c646e086a583e137ed273b27f87f6
mikenic <temperanceaubie@gmail.com>
2017-11-07 22:24:22 -0800

    downloadpost fixes

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -4,14 +4,30 @@ module Api
 			#skip_before_action :verify_authenticity_token
 			respond_to :json
 			
-			
-
-
 			def index
 				#all posts
 				#respond_with Post.all
 				@posts = Post.all
 				render json: {status: 'SUCCESS', message:'Loaded Posts', data:@posts}, status: :ok
+			
+
+
+##################################
+
+				@posts = Post.all
+				@all_posts = Post.all
+				@all_posts.each do |post|
+					if current_user.not_friends_with? post.user.id || current_user.id == post.user.id
+						@posts -= [post]
+					else
+						#we are friends
+
+					end
+				end
+				render json: {status: 'SUCCESS', message:'Loaded Posts', data:@posts}, status: :ok
+
+
+
 			end
 
 			def show
@@ -20,32 +36,10 @@ module Api
 				render json: {status: 'SUCCESS', message:'Loaded Post', data:@post}, status: :ok
 			end
 
-			def create
-				#respond_with Post.create(params[:friendship])			
-				#@user = User.where(email: params[:user]).first
-				##@all_users = User.all
-				##@api_user = @all_users.find_by_email(params[:email])
-				#@api_user = @all_users.first
-				#if (@user = User.find_by_email(params[:user][:email])) && @user.valid_password?(params[:user][:password])
+			def create		
 				@api_user = @current_user
 				if @api_user
-					#@post = new Post(post_params)
-
-					#@post = Post.create(post_params)
-
-					
-
-					#@image = decode_image_data(params[:image_data])
-
-					#@api_user.posts.build(post_params)
 					@post = @api_user.posts.build(post_params)
-					#@post.image = @image
-
-
-
-					#@post_image = decode_image_data(post_params[:image_data])
-					
-					#@post = @api_user.posts.build(post_params)
 					if @post.save #@post.save
 						render json: {status: 'SUCCESS', message:'Saved Post', data:@post}, status: :ok
 					else
@@ -67,23 +61,7 @@ module Api
 			def post_params
 				json = JSON.parse(request.raw_post)
 				params = ActionController::Parameters.new(json)
-				#@image = decode_image_data(params[:image])
-				params.permit(:title, :content, :longitude, :latitude, :image)### fix
-				#params[:image] = @image
-				#@sent_image_data = params[:image]
-				#params[:image] = decode_image_data(@sent_image_data)
-			end
-
-			def decode_image_data my_data
-				# decode the base64
-				data = StringIO.new(Base64.decode64(my_data))
-				# assign some attributes for carrierwave processing
-				data.class.class_eval { attr_accessor :original_filename, :content_type }
-				data.original_filename = SecureRandom.hex(16) + ".png"
-				data.content_type = "image/png"
-				#self.image = data
-				# return decoded data
-				data
+				params.permit(:title, :content, :longitude, :latitude, :image)
 			end
 		end
 	end
