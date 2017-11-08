30495f9e1e3be1ada1e42c762b8f0bb46f522914
mikenic <temperanceaubie@gmail.com>
2017-11-07 16:37:36 -0800

    api fixes

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -63,7 +63,7 @@ module Api
 			def post_params
 				json = JSON.parse(request.raw_post)
 				params = ActionController::Parameters.new(json)
-				params.require(:post).permit(:title, :content, :longitude, :latitude, :image_data, :image)### fix
+				params.permit(:title, :content, :longitude, :latitude, :image_data, :image)### fix
 				#params[:image] = decode_image_data(params[:image_data])
 			end
 
